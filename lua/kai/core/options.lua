61573d8660239cbaa7269ef1738d19d0beb223e0
zhangzongkai <zhangzongkai@lixiang.com>
2023-03-02 16:40:06 +0800

    feat: add core option

--- lua/kai/core/options.lua
+++ lua/kai/core/options.lua
@@ -1,28 +1,39 @@
-local opt = vim.opt         -- for conciseness
+-------------------------------------------------------------------------------
+-- For conciseness
+-------------------------------------------------------------------------------
+local opt = vim.opt
+local wo = vim.wo
 
+
+-------------------------------------------------------------------------------
+-- Line settings
+-------------------------------------------------------------------------------
 -- line numbers
 opt.relativenumber = true   -- show relative line numbers
 opt.number = true           -- shows absolute line number on cursor line (when relative number is on)
 
--- tabs & indentation
+-- line wrapping
+opt.wrap = false            -- disable line wrapping
+
+-- cursor line
+opt.cursorline = true       -- highlight the current cursor line
+wo.colorcolumn = "120"      -- set line width identifier
+
+-------------------------------------------------------------------------------
+-- Tabs & Indentation
+-------------------------------------------------------------------------------
 opt.tabstop = 4             -- spaces for tabs (prettier default)
 opt.shiftwidth = 4          -- spaces for indent width
 opt.expandtab = true        -- expand tab to spaces
 opt.autoindent = true       -- copy indent from current line when starting new one
 
--- line wrapping
-opt.wrap = false            -- disable line wrapping
-
 -- search settings
 opt.ignorecase = true       -- ignore case when searching
 opt.smartcase = true        -- if you include mixed case in your search, assumes you want case-sensitive
 
--- cursor line
-opt.cursorline = true       -- highlight the current cursor line
-
---
--- appearance
---
+-------------------------------------------------------------------------------
+-- Appearance
+-------------------------------------------------------------------------------
 
 -- turn on termguicolors for nightfly colorscheme to work
 -- (have to use iterm2 or any other true color terminal)
@@ -36,9 +47,19 @@ opt.backspace = "indent,eol,start"  -- allow backspace on indent, end of line or
 -- clipboard
 opt.clipboard:append("unnamedplus") -- use system clipboard as default register
 
+-------------------------------------------------------------------------------
+-- Windows
+-------------------------------------------------------------------------------
 -- split windows
 opt.splitright = true       -- split vertical window to the right
 opt.splitbelow = true       -- split horizontal window to the bottom
 
 opt.iskeyword:append("-")   -- consider string-string as whole word
+
+-------------------------------------------------------------------------------
+-- File
+-------------------------------------------------------------------------------
+opt.backup = false
+opt.writebackup = false
+opt.swapfile = false
 
